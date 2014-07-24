f47ebb8d124fae651b07ad09e1ee75aad552257b
Ben Plum <mr@benplum.com>
2014-07-24 17:38:13 -0400

    Adding grunt build process

---
+++ 13/app/Gruntfile.js
@@ -0,0 +1,36 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		meta: {
+			banner: '/* \n' +
+					' * <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+					' */'
+		},
+		// CSSMin
+		cssmin: {
+			combine: {
+				files: '<%= pkg.css %>'
+			}
+		},
+		// Uglify
+		uglify: {
+			options: {
+				banner: '<%= meta.banner %>',
+				report: 'min'
+			},
+			target: {
+				files: '<%= pkg.js %>'
+			}
+		}
+	});
+
+	// Load tasks
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	// Default task.
+	grunt.registerTask('default', [ 'uglify', 'cssmin' ]);
+
+};
